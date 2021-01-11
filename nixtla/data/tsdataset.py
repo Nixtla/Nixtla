Corrected Xfuture leakage in TSLOADERS, tested multivariate model

nixtla/data/tsdataset.py
@@ -22,7 +22,7 @@ class TimeSeriesDataset(Dataset):
                  Y_df: pd.DataFrame,
                  X_df: pd.DataFrame=None,
                  S_df: pd.DataFrame=None,
-                 mask_df: list=None,
+                 mask_df: pd.DataFrame=None,
                  f_cols: list=None):
         """
         """
@@ -34,6 +34,10 @@ class TimeSeriesDataset(Dataset):
 
         print('Processing dataframes ...')
         # Pandas dataframes to data lists
+        if mask_df is None:
+            mask_df = Y_df[['unique_id', 'ds']].copy()
+            mask_df['mask'] = np.ones(len(Y_df))
+
         ts_data, s_data, self.meta_data, self.t_cols, self.X_cols \
                          = self._df_to_lists(Y_df=Y_df, S_df=S_df, X_df=X_df, mask_df=mask_df)
 

nixtla/data/tsloader_fast.py
@@ -53,6 +53,10 @@ class TimeSeriesLoader(object):
         insample_condition = t.sum(self.ts_windows[:, self.t_cols.index('insample_mask'), :self.input_size], axis=1)
         sampling_idx = t.nonzero(outsample_condition * insample_condition > 0) #element-wise product
         sampling_idx = list(sampling_idx.flatten().numpy())
+
+        # TODO: pensar caso hipotético de validación a la Hyndman, puede pasar que sea un bug por samplear
+        #.      todas las series de validación y usar esta condición
+        assert len(sampling_idx)>0, 'Check the data and masks as sample_idxs are empty'
         return sampling_idx
 
     def _create_windows_tensor(self):
@@ -63,32 +67,21 @@ class TimeSeriesLoader(object):
         # Memory efficiency is gained from keeping across dataloaders common ts_tensor in dataset
         # Filter function is used to define train tensor and validation tensor with the offset
         # Default ts_idxs=ts_idxs sends all the data
-
-        # ANTES
-        #tensor, right_padding, train_mask = self.ts_dataset.get_filtered_ts_tensor(offset=self.offset, output_size=self.output_size,
-        #                                                                           window_sampling_limit=self.window_sampling_limit)
-
-        # Outsample mask checks existance of values in ts, train_mask mask is used to filter out validation
-        # is_train_loader inverts the train_mask in case the dataloader is in validation mode
-        # elementwise multiplication (#n_batch, #n_time) x (#n_batch, #n_time)
-        #mask = train_mask if self.is_train_loader else (1 - train_mask)
-        #tensor[:, self.t_cols.index('outsample_mask'), :] = tensor[:, self.t_cols.index('outsample_mask'), :] * mask
-
-        # AHORA
         tensor, right_padding = self.ts_dataset.get_filtered_ts_tensor(offset=self.offset, output_size=self.output_size,
                                                                         window_sampling_limit=self.window_sampling_limit)
         tensor = t.Tensor(tensor)
 
+        # Outsample mask checks existance of values in ts, train_mask mask is used to filter out validation
+        # is_train_loader inverts the train_mask in case the dataloader is in validation mode
         if not self.is_train_loader:
             tensor[:, self.t_cols.index('outsample_mask'), :] = (1-tensor[:, self.t_cols.index('outsample_mask'), :])
 
         padder = t.nn.ConstantPad1d(padding=(self.input_size, right_padding), value=0)
         tensor = padder(tensor)
 
-        # ANTES
-        # Last output_size outsample_mask and y to 0
-        #tensor[:, self.t_cols.index('y'), -self.output_size:] = 0 # overkill to ensure no validation leakage
-        #tensor[:, self.t_cols.index('outsample_mask'), -self.output_size:] = 0
+        # Since Xt from future is returned in tensor, protection from leakage is needed
+        tensor[:, self.t_cols.index('y'), -self.output_size:] = 0 # overkill to ensure no validation leakage
+        tensor[:, self.t_cols.index('outsample_mask'), -self.output_size:] = 0
 
         # Creating rolling windows and 'flattens' them
         windows = tensor.unfold(dimension=-1, size=self.input_size + self.output_size, step=self.idx_to_sample_freq)
@@ -101,7 +94,7 @@ class TimeSeriesLoader(object):
         # Broadcast s_matrix: This works because unfold in windows_tensor, orders: time, serie
         s_matrix = self.ts_dataset.s_matrix.repeat(repeats=int(len(windows)/self.ts_dataset.n_series), axis=0)
 
-        return windows, s_matrix
+        return windows, s_matrix, tensor
 
     def __len__(self):
         return len(self.len_series)
@@ -113,8 +106,6 @@ class TimeSeriesLoader(object):
         else:
             sample_idxs = self.windows_sampling_idx
 
-        assert len(sample_idxs)>0, 'Check the data and masks as sample_idxs are empty'
-
         n_batches = int(np.ceil(len(sample_idxs) / self.batch_size)) # Must be multiple of batch_size for paralel gpu
 
         for idx in range(n_batches):
@@ -124,13 +115,16 @@ class TimeSeriesLoader(object):
 
     def __get_item__(self, index):
         if self.model == 'nbeats':
-            return self._nbeats_batch(index)
+            return self._windows_batch(index)
         elif self.model == 'esrnn':
-            assert 1<0, 'hacer esrnn'
+            #return self._full_series_batch(index)
+            assert 1<0, 'Hierarchical sampling not implemented'
         else:
             assert 1<0, 'error'
 
-    def _nbeats_batch(self, index):
+    def _windows_batch(self, index):
+        """ NBEATS, TCN models """
+
         # Access precomputed rolling window matrix (RAM intensive)
         windows = self.ts_windows[index]
         s_matrix = self.s_matrix[index]
@@ -148,12 +142,35 @@ class TimeSeriesLoader(object):
                  'outsample_y': outsample_y, 'outsample_x':outsample_x, 'outsample_mask':outsample_mask}
         return batch
 
+    # def _full_series_batch(self, index):
+    #     """ ESRNN, RNN models """
+
+    #     print("[index]", index)
+    #     print("self.ts_tensor.shape", self.ts_tensor.shape)
+
+    #     ts_tensor = self.ts_tensor[index]
+
+    #     # Trim batch to shorter time series to avoid zero padding
+    #     insample_y = ts_tensor[:, self.t_cols.index('y'), :]
+    #     batch_len_series = np.array(self.ts_dataset.len_series)[index]
+    #     min_batch_len = np.min(batch_len_series)
+    #     insample_y = insample_y[:, -min_batch_len:]
+
+    #     insample_x = ts_tensor[:, self.t_cols.index('y')+1:self.t_cols.index('insample_mask'), :]
+    #     insample_x = insample_x[:, -min_batch_len:]
+
+    #     s_matrix = self.ts_dataset.s_matrix[index]
+
+    #     batch = {'insample_y': insample_y, 'idxs': index, 'insample_x': insample_x, 's_matrix': s_matrix}
+
+    #     return batch
+
     def _create_train_data(self):
         """
         """
-        #print('Creating windows matrix ...')
+        # print('Creating windows matrix ...')
         # Create rolling window matrix for fast information retrieval
-        self.ts_windows, self.s_matrix = self._create_windows_tensor()
+        self.ts_windows, self.s_matrix, _ = self._create_windows_tensor()
         self.n_windows = len(self.ts_windows)
         self.windows_sampling_idx = self._update_sampling_windows_idxs()
 

nixtla/data/tsloader_general.py
@@ -13,13 +13,6 @@ from .tsdataset import TimeSeriesDataset
 from collections import defaultdict
 
 # Cell
-# TODO: revisar _create_windows_tensor method para checar que la declaracion de la train_mask y su uso es correcto
-#.      no sabemos si hay algo diferente respecto a su uso en el DataLoaderFast
-# TODO: pensar variable shuffle para compatibilidad con dataloader de pytorch y keras
-#.      por el momento tenemos solo validacion boostrapeada, no existe modo no shuffle
-#.      para evaluacion no estocástica, nuestra validación está hackeada.
-# TODO: _get_sampleable_windows_idxs linea muy malvada de windows con frequencia aumenta de 3.0 a 3.6 segundos el batch
-
 class TimeSeriesLoader(object):
     def __init__(self,
                  ts_dataset: TimeSeriesDataset,
@@ -67,6 +60,10 @@ class TimeSeriesLoader(object):
         insample_condition = t.sum(ts_windows_flatten[:, self.t_cols.index('insample_mask'), :self.input_size], axis=1)
         sampling_idx = t.nonzero(outsample_condition * insample_condition > 0) #element-wise product
         sampling_idx = list(sampling_idx.flatten().numpy())
+
+        # TODO: pensar caso hipotético de validación a la Hyndman, puede pasar que sea un bug por samplear
+        #.      todas las series de validación y usar esta condición
+        assert len(sampling_idx)>0, 'Check the data and masks as sample_idxs are empty'
         return sampling_idx
 
     def _create_windows_tensor(self, ts_idxs=None):
@@ -76,20 +73,20 @@ class TimeSeriesLoader(object):
         """
         # Filter function is used to define train tensor and validation tensor with the offset
         # Default ts_idxs=ts_idxs sends all the data, otherwise filters series
-        tensor, right_padding, train_mask = self.ts_dataset.get_filtered_ts_tensor(offset=self.offset, output_size=self.output_size,
-                                                                                   window_sampling_limit=self.window_sampling_limit,
-                                                                                   ts_idxs=ts_idxs)
+        tensor, right_padding = self.ts_dataset.get_filtered_ts_tensor(offset=self.offset, output_size=self.output_size,
+                                                                       window_sampling_limit=self.window_sampling_limit,
+                                                                       ts_idxs=ts_idxs)
         tensor = t.Tensor(tensor)
 
         # Outsample mask checks existance of values in ts, train_mask mask is used to filter out validation
         # is_train_loader inverts the train_mask in case the dataloader is in validation mode
-        mask = train_mask if self.is_train_loader else (1 - train_mask)
-        tensor[:, self.t_cols.index('outsample_mask'), :] = tensor[:, self.t_cols.index('outsample_mask'), :] * mask
+        if not self.is_train_loader:
+            tensor[:, self.t_cols.index('outsample_mask'), :] = (1-tensor[:, self.t_cols.index('outsample_mask'), :])
 
         padder = t.nn.ConstantPad1d(padding=(self.input_size, right_padding), value=0)
         tensor = padder(tensor)
 
-        # Last output_size outsample_mask and y to 0
+        # Since Xt from future is returned in tensor, protection from leakage is needed
         tensor[:, self.t_cols.index('y'), -self.output_size:] = 0 # overkill to ensure no validation leakage
         tensor[:, self.t_cols.index('outsample_mask'), -self.output_size:] = 0
 
@@ -104,7 +101,7 @@ class TimeSeriesLoader(object):
         # Broadcast s_matrix: This works because unfold in windows_tensor, orders: serie, time
         s_matrix = self.ts_dataset.s_matrix[ts_idxs]
         windows_per_serie = len(windows)//len(ts_idxs)
-        s_matrix = s_matrix.repeat(windows_per_serie, 0)
+        s_matrix = s_matrix.repeat(repeats=windows_per_serie, axis=0)
 
         return windows, s_matrix
 
@@ -157,9 +154,6 @@ class TimeSeriesLoader(object):
         outsample_y = windows[:, self.t_cols.index('y'), self.input_size:]
         outsample_x = windows[:, (self.t_cols.index('y')+1):self.t_cols.index('insample_mask'), self.input_size:]
         outsample_mask = windows[:, self.t_cols.index('outsample_mask'), self.input_size:]
-
-        assert all(insample_mask.sum(axis=1)>0), 'Insample mask contains windows with all zeros.'
-        assert all(outsample_mask.sum(axis=1)>0), 'Outsample mask contains windows with all zeros.'
 
         batch = {'s_matrix': s_matrix,
                  'insample_y': insample_y, 'insample_x':insample_x, 'insample_mask':insample_mask,

nixtla/experiments/nbeats/hyperopt_epf.py
@@ -100,18 +100,13 @@ def run_val_nbeatsx(mc, train_loader, val_loader, trials, trials_file_name, fina
     model.fit(train_ts_loader=train_loader, val_ts_loader=val_loader, verbose=True, eval_steps=10) # aqui val_loader==Test
 
     # TODO: Pytorch numerical error hacky protection, protect from losses.numpy.py
-    hyperopt_reported_loss = model.final_outsample_loss
-    if np.isnan(model.final_insample_loss):
-        hyperopt_reported_loss = 100
-    if model.final_insample_loss<=0:
-        hyperopt_reported_loss = 100
-
-    if np.isnan(model.final_outsample_loss):
-        hyperopt_reported_loss = 100
-    if model.final_outsample_loss<=0:
+    hyperopt_reported_loss = 100
+    if (not np.isnan(model.final_insample_loss)) and (not np.isnan(model.final_outsample_loss)):
+        hyperopt_reported_loss = model.final_outsample_loss
+    if (model.final_insample_loss<=0) or (model.final_outsample_loss<=0):
         hyperopt_reported_loss = 100
 
-    results =  {'loss': model.final_outsample_loss,
+    results =  {'loss': hyperopt_reported_loss,
                 'loss_name': mc['val_loss'], #val_mae <--------
                 'mc': mc,
                 'final_insample_loss': model.final_insample_loss,