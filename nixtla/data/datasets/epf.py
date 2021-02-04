2e915b27d1a03679ae2da841e46c983e846874bb
Fede <fede.garza.ramirez@gmail.com>
2021-02-03 19:44:30 -0600

    fixing tests

--- nixtla/data/datasets/epf.py
+++ nixtla/data/datasets/epf.py
@@ -15,7 +15,6 @@ from pandas.tseries.frequencies import to_offset
 from .utils import download_file, Info, TimeSeriesDataclass
 from ..tsdataset import TimeSeriesDataset
 
-
 # Cell
 SOURCE_URL = 'https://sandbox.zenodo.org/api/files/da5b2c6f-8418-4550-a7d0-7f2497b40f1b/'
 
@@ -52,12 +51,11 @@ EPFInfo = Info(groups=('NP', 'PJM', 'BE', 'FR', 'DE'),
 # Cell
 class EPF:
 
-    #@staticmethod
+    @staticmethod
     def load(directory: str,
-             group: str):
-             #is_training: bool,
-             #days_in_test: int,
-             #return_tensor: bool = True): # -> Union[TimeSeriesDataset, TimeSeriesDataclass]
+             group: str) -> Tuple[pd.DataFrame,
+                                  Optional[pd.DataFrame],
+                                  Optional[pd.DataFrame]]:
         """
         Downloads and loads EPF data.
 
@@ -68,14 +66,6 @@ class EPF:
         group: str
             Group name.
             Allowed groups: 'NP', 'PJM', 'BE', 'FR', 'DE'.
-        training: bool
-            Wheter return training or testing data. Default True.
-        days_in_test: int
-            Number of days to consider in test.
-            Only used when training=True.
-        return_tensor: bool
-            Wheter return TimeSeriesDataset (tensors, True) or
-            TimeSeriesDataclass (dataframes)
         """
         path = Path(directory) / 'epf' / 'datasets'
 
@@ -100,33 +90,17 @@ class EPF:
         dummies_cols = [col for col in df \
                         if (col.startswith('day') or col.startswith('hour_'))]
 
-        # last_date_test = pd.to_datetime(class_group.test_date) + \
-        #                     timedelta(days=days_in_test)
-
-        # if is_training:
-        #     df = df.query('ds < @class_group.test_date')
-        # else:
-        #     last_date_test = pd.to_datetime(class_group.test_date) + \
-        #                      timedelta(days=days_in_test)
-        #     df = df.query('ds >= @class_group.test_date')
-
         Y = df.filter(items=['unique_id', 'ds', 'y'])
         X = df.filter(items=['unique_id', 'ds', 'Exogenous1', 'Exogenous2', 'week_day'] + \
                       dummies_cols)
 
-    #def get_data(self):
-        return Y, X
-        # if return_tensor:
-        #     return TimeSeriesDataset(y_df=Y, X_s_df=None, X_t_df=X, ts_train_mask=ts_train_mask)
-        # else:
-        #     return TimeSeriesDataclass(Y=Y, S=None, X=X, group=group)
+        return Y, X, None
 
     @staticmethod
     def load_groups(directory: str,
-                    groups: List[str]): # = ['BE', 'FR'],
-                    #is_training: bool,
-                    #days_in_test: int,
-                    #return_tensor: bool = True): # -> Union[TimeSeriesDataset, TimeSeriesDataclass]:
+                    groups: List[str]) -> Tuple[pd.DataFrame,
+                                                Optional[pd.DataFrame],
+                                                Optional[pd.DataFrame]]:
         """
         Downloads and loads panel of EPF data
         according of groups.
@@ -138,23 +112,14 @@ class EPF:
         groups: List[str]
             Group names.
             Allowed groups: 'NP', 'PJM', 'BE', 'FR', 'DE'.
-        training: bool
-            Wheter return training or testing data. Default True.
-        days_in_test: int
-            Number of days to consider in test.
-            Only used when training=True.
-        return_tensor: bool
-            Wheter return TimeSeriesDataset (tensors, True) or
-            TimeSeriesDataclass (dataframes)
         """
         Y = []
         X = []
         for group in groups:
-            Y_df, X_df = EPF.load(directory=directory, group=group)
-                                  #is_training=is_training, days_in_test=days_in_test,
-                                  #return_tensor=False)
+            Y_df, X_df, S_df = EPF.load(directory=directory, group=group)
             Y.append(Y_df)
             X.append(X_df)
+
         Y = pd.concat(Y).sort_values(['unique_id', 'ds']).reset_index(drop=True)
         X = pd.concat(X).sort_values(['unique_id', 'ds']).reset_index(drop=True)
 
@@ -162,11 +127,7 @@ class EPF:
         dummies = pd.get_dummies(S['unique_id'], prefix='static')
         S = pd.concat([S, dummies], axis=1)
 
-        # if return_tensor:
         return Y, X, S
-        #     return TimeSeriesDataset(y_df=Y, X_s_df=None, X_t_df=X)
-        # else:
-        #     return TimeSeriesDataclass(Y=Y, S=S, X=X, group=groups)
 
     @staticmethod
     def download(directory: str) -> None:
